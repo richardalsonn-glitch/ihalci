159f4b43a4655a54ed51fdc76e65272987a7d3e2
richardalsonn-glitch <richardalsonn@gmail.com>
2026-04-02 15:25:33 +0300

    Update routers.js

--- routers/routers.js
+++ routers/routers.js
@@ -1,15 +1,30 @@
-/opt/render/project/src/routers/routers.js:7
-router.get('/jeton-paketleri', async (req, res) => {
-^
-ReferenceError: router is not defined
-    at Object.<anonymous> (/opt/render/project/src/routers/routers.js:7:1)
-    at Module._compile (node:internal/modules/cjs/loader:1706:14)
-    at Object..js (node:internal/modules/cjs/loader:1839:10)
-    at Module.load (node:internal/modules/cjs/loader:1441:32)
-    at Function._load (node:internal/modules/cjs/loader:1263:12)
-Menu
-    at TracingChannel.traceSync (node:diagnostics_channel:328:14)
-    at wrapModuleLoad (node:internal/modules/cjs/loader:237:24)
-    at Module.require (node:internal/modules/cjs/loader:1463:12)
-    at require (node:internal/modules/helpers:147:16)
-    at Object.<anonymous> (/opt/render/project/src/app.js:127:1)
+module.exports = function () {
+
+    require('../functions/middleware')(this);
+
+    app.use('/', require('../routers/home/index'));
+    app.use('/', require('../routers/admin/index'));
+
+    app.get('/jeton-paketleri', async (req, res) => {
+        res.render('home/pages/jeton-paketleri', {
+            site,
+            user: req.user || null,
+            cuzdan: typeof cuzdan !== 'undefined' ? cuzdan : null,
+            kategoriler: typeof kategoriler !== 'undefined' ? kategoriler : [],
+            altkategoriler: typeof altkategoriler !== 'undefined' ? altkategoriler : []
+        });
+    });
+
+    app.get('/jeton-paketleri/250-jeton', async (req, res) => {
+        res.redirect('/jeton-paketleri');
+    });
+
+    app.get('/jeton-paketleri/500-jeton', async (req, res) => {
+        res.redirect('/jeton-paketleri');
+    });
+
+    app.get('/jeton-paketleri/900-jeton', async (req, res) => {
+        res.redirect('/jeton-paketleri');
+    });
+
+}
