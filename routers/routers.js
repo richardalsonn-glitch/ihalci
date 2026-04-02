7ee77632482793f15c4e5d109ec6b50d3e51cf9e
richardalsonn-glitch <richardalsonn@gmail.com>
2026-04-02 15:11:52 +0300

    Add routes for jeton-paketleri and redirects

--- routers/routers.js
+++ routers/routers.js
@@ -4,3 +4,24 @@ module.exports = function () {
     app.use('/', require('../routers/home/index'));
     app.use('/', require('../routers/admin/index'));
 }
+router.get('/jeton-paketleri', async (req, res) => {
+    res.render('home/pages/jeton-paketleri', {
+        site,
+        user: req.user || null,
+        cuzdan: typeof cuzdan !== 'undefined' ? cuzdan : null,
+        kategoriler: typeof kategoriler !== 'undefined' ? kategoriler : [],
+        altkategoriler: typeof altkategoriler !== 'undefined' ? altkategoriler : []
+    });
+});
+
+router.get('/jeton-paketleri/250-jeton', async (req, res) => {
+    res.redirect('/jeton-paketleri');
+});
+
+router.get('/jeton-paketleri/500-jeton', async (req, res) => {
+    res.redirect('/jeton-paketleri');
+});
+
+router.get('/jeton-paketleri/900-jeton', async (req, res) => {
+    res.redirect('/jeton-paketleri');
+});
