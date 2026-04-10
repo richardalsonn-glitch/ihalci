f6af4424301a291198c1a5afb80d6622003e9e2f
richardalsonn-glitch <richardalsonn@gmail.com>
2026-04-10 11:27:21 +0300

    Update teklifler.js

--- database/teklifler.js
+++ database/teklifler.js
@@ -1,12 +1,11 @@
 const teklifler = new Schema({
-    teklif:{type:Number},
+    teklif:{type:Number},           // TL cinsinden anlık fiyat
+    jetonMiktari:{type:Number},     // bu teklifte harcanan jeton
     uyeId: { type: Schema.Types.ObjectId, ref: 'uyeler' },
     urunId:{type:mongoose.Schema.Types.ObjectId, required:true },
     teklifdurum:{type:Boolean, default:true},
     tekrarteklif:{type:Boolean, default:true},
     eklenmetarihi: { type: String, default: moment().format('YYYY-MM-DD HH:mm:ss') }
-
-
 }, {collection:'teklifler'});
 
 module.exports = mongoDB.model('teklifler', teklifler);
